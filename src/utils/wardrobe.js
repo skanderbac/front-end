85a072c80336f5183565b2123470d35697e49a0c
skanderbac <skanderbac@users.noreply.github.com>
2021-05-19 05:07:57 +0100

    aaa

--- src/utils/wardrobe.js
+++ src/utils/wardrobe.js
@@ -1,7 +1,7 @@
 import axios from "axios";
-const url = 'https://online-wardrobe-back.netlify.app/robe' ;
+const url = 'https://online-wardrobe-back.herokuapp.com/robe' ;
 export const ajoutdress =  (newdress) =>   axios.post(url , newdress);
 export const getDresses =  () =>   axios.get(url);
 export const deleteDresses =  (id) =>   axios.delete(`${url},${id}`);
 export const getPreferences =  (usage,gender) =>   axios.get('http://51.38.224.143:9000/api?usage='+usage+'&&gender='+gender);
-export const getClasification =  (image) =>   axios.post('http://127.0.0.1:8000/api/predict/',image);
+export const getClasification =  (image) =>   axios.post('http://51.38.224.143:8000/api/predict/',image);
