5568272a46d975ef01cb0bd6c4d3b087154f19f5
skanderbac <skanderbac@users.noreply.github.com>
2021-05-19 01:25:07 +0100

    gfbdfv

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import { BrowserRouter as Router} from 'react-router-dom';
+import { HashRouter as Router} from 'react-router-dom';
 import store from "./Redux/store";
 import {Provider} from "react-redux";
 import App from './App';
